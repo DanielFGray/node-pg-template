2b636725ea084ce9dae5e3172e64f2b8c8973185
DanielFGray <danielfgray@gmail.com>
2024-12-12 12:10:16 -0600

    chore: move SocialLogin -> components

--- src/register.tsx
+++ src/register.tsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useSearchParams } from 'react-router'
 import { useAuth } from './Auth.ctx.js'
 import type { FormResult, User } from './types.js'
 import { api } from './api.js'
-import { SocialLogin } from './SocialLogin.js'
+import { SocialLogin } from './components.js'
 import { register as validator } from './schemas.js'
 
 export default function Register() {
