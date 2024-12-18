117cf0f8f0674f98ece79d5ebccf95cf08007a77
DanielFGray <danielfgray@gmail.com>
2024-12-18 01:25:48 -0600

    chore: use node:http directly and forward to express this should make it easier to upgrade websocket requests later

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import http from 'node:http'
 import { app } from './app.js'
 import log from './log.js'
 import { env } from './assertEnv.js'
@@ -6,4 +7,6 @@ if (process.env.NODE_ENV !== 'production') {
   import('./cypress.js').then(m => m.installCypressCommands(app))
 }
 
-app.listen(env.PORT, () => log.info(`server listening on port ${env.PORT}`))
+const server = http.createServer(app.handle.bind(app))
+
+app.listen(env.PORT, () => log.info('server listening on %O', server.address()))
